bea526d02e5c1aa77e2488de85063a524fb1f07d
Nelson Steve <nelsonsteve.business@gmail.com>
2024-10-02 22:08:04 +0500

    Update

--- Brisk/src/Engine/Renderer/Renderer.hpp
+++ Brisk/src/Engine/Renderer/Renderer.hpp
@@ -2,11 +2,6 @@
 
 namespace Brisk 
 {
-	//struct RenderTarget {
-	//	//VkDescriptorSet pDescriptorSet;
-	//	BriskTexture* pTexture;
-	//};
-
 	template<typename RendererClass>
 	class Renderer {
 	public:
@@ -14,21 +9,4 @@ namespace Brisk
 			RendererClass::Init();		
 		}
 	};
-
-	//class Renderer {
-	//public:
-	//	virtual ~Renderer() {};
-
-	//	virtual void Create() = 0;
-	//	virtual void Release() = 0;
-
-	//	virtual void AddRenderTarget(RenderTarget renderTarget) = 0;
-
-	//	virtual void SetupRenderingPipeline(Swapchain* swapchain) = 0;
-
-	//	virtual void PreRender() = 0;
-	//	virtual void Render() = 0;
-	//	virtual void PostRender() = 0;
-	//	virtual void WaitDeviceIdle() = 0;
-	//};
 }

bf85d2d1a84b32a9174f6e36ecb6fc9ed9a293ca
Nelson Steve <nelsonsteve.business@gmail.com>
2024-11-27 23:21:28 +0500

    Contructor and GetTag function for Entity

---
+++ Brisk/src/Engine/Entity.cpp
@@ -0,0 +1,23 @@
+#include "Entity.hpp"
+#include "Component.hpp"
+
+#define STATIC(a) decltype(a) a
+
+STATIC(Brisk::Entity::m_Scene);
+
+#undef STATIC(a) decltype(a) a
+
+namespace Brisk 
+{
+	Entity::Entity(entt::entity handle, Scene* scene)
+		: m_EntityHandle(handle)
+	{
+		m_Scene = scene;
+	}
+
+	const std::string& Entity::GetName()
+	{
+		return GetComponent<TagComponent>().Tag;
+	}
+
+}

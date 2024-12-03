5dd911bf3b9b2a6897cf456dbf3141492ebafd20
Nelson Steve <nelsonsteve.business@gmail.com>
2024-12-03 23:00:44 +0500

    Update

--- Brisk/src/Engine/SceneSerializer.hpp
+++ Brisk/src/Engine/SceneSerializer.hpp
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "Scene.hpp"
-#include "Core/Core.hpp"
-
-namespace Brisk {
-
-	class SceneSerializer {
-	public:
-		SceneSerializer(const Ref<Scene> scene);
-
-		void Serialize(const std::string& filepath);
-		void SerializeRuntime(const std::string& filepath);
-
-		bool DeserializeRuntime(const std::string& filepath);
-		bool Deserialize(const std::string& filepath);
-
-	private:
-		Ref<Scene> m_Scene;
-	};
-
-}
+//#pragma once
+//
+//#include "Scene.hpp"
+//#include "Core/Core.hpp"
+//
+//namespace Brisk {
+//
+//	class SceneSerializer {
+//	public:
+//		SceneSerializer(const Ref<Scene> scene);
+//
+//		void Serialize(const std::string& filepath);
+//		void SerializeRuntime(const std::string& filepath);
+//
+//		bool DeserializeRuntime(const std::string& filepath);
+//		bool Deserialize(const std::string& filepath);
+//
+//	private:
+//		Ref<Scene> m_Scene;
+//	};
+//
+//}

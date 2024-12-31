55b27fcbc6bf0867c392b1cf74dc41854dbf2808
Nelson Steve <nelsonsteve.business@gmail.com>
2024-12-31 17:22:31 +0500

    Update

--- Brisk/src/Graphics/Vulkan/ShaderVulkan.cpp
+++ Brisk/src/Graphics/Vulkan/ShaderVulkan.cpp
@@ -5,7 +5,8 @@
 
 namespace Brisk
 {
-	void ShaderVulkan::Init(std::pair<std::string, Pipeline::ShaderStage> shaderInfo) {
+	void ShaderVulkan::Init(std::pair<std::string, Pipeline::ShaderStage> shaderInfo)
+	{
 		m_Module = UtilitiesVulkan::CreateShaderModule(std::static_pointer_cast<GpuAdapterVulkan>(Engine::s_Application->GetGpuAdapter())->GetDevice(), shaderInfo.first);
 		m_ShaderStage.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
 		if (shaderInfo.second == Pipeline::ShaderStage::VERTEX)
@@ -18,7 +19,8 @@ namespace Brisk
 		m_ShaderStage.pName = "main";
 	}
 
-	void ShaderVulkan::Allocate(std::shared_ptr<Pipeline> pipeline) {
+	void ShaderVulkan::Allocate(std::shared_ptr<Pipeline> pipeline)
+	{
 		VkDescriptorSetAllocateInfo allocInfo{};
 		allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
 		allocInfo.descriptorPool = m_descriptor_pools.scene;
@@ -30,39 +32,74 @@ namespace Brisk
 		}
 	}
 
-	void ShaderVulkan::UpdateResources() {
-
+	void ShaderVulkan::UpdateResources()
+	{
 	}
 
-	void ShaderVulkan::Bind() {
-
+	void ShaderVulkan::Bind()
+	{
 	}
 
-	void ShaderVulkan::SetPipeline(std::shared_ptr<Pipeline> pipeline) {
-
+	void ShaderVulkan::SetPipeline(std::shared_ptr<Pipeline> pipeline)
+	{
 	}
 
-	void ShaderVulkan::SetAlbedoTexture(std::shared_ptr<Texture> texture) {
-
+	void ShaderVulkan::SetAlbedoTexture(std::shared_ptr<Texture> texture)
+	{
+		VkWriteDescriptorSet descriptorWrite;
+		descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrite.dstSet = m_DescriptorSet;
+		descriptorWrite.dstBinding = 0;
+		descriptorWrite.descriptorCount = 1;
+		descriptorWrite.pImageInfo = &std::static_pointer_cast<TextureVulkan>(texture)->GetDescriptor();
 	}
 
-	void ShaderVulkan::SetNormalTexture(std::shared_ptr<Texture> texture) {
-
+	void ShaderVulkan::SetNormalTexture(std::shared_ptr<Texture> texture)
+	{
+		VkWriteDescriptorSet descriptorWrite;
+		descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrite.dstSet = m_DescriptorSet;
+		descriptorWrite.dstBinding = 1;
+		descriptorWrite.descriptorCount = 1;
+		descriptorWrite.pImageInfo = &std::static_pointer_cast<TextureVulkan>(texture)->GetDescriptor();
 	}
 
-	void ShaderVulkan::SetMetallicTexture(std::shared_ptr<Texture> texture) {
-
+	void ShaderVulkan::SetMetallicTexture(std::shared_ptr<Texture> texture)
+	{
+		VkWriteDescriptorSet descriptorWrite;
+		descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrite.dstSet = m_DescriptorSet;
+		descriptorWrite.dstBinding = 2;
+		descriptorWrite.descriptorCount = 1;
+		descriptorWrite.pImageInfo = &std::static_pointer_cast<TextureVulkan>(texture)->GetDescriptor();
 	}
 
-	void ShaderVulkan::SetOcclusionTexture(std::shared_ptr<Texture> texture) {
-
+	void ShaderVulkan::SetOcclusionTexture(std::shared_ptr<Texture> texture)
+	{
+		VkWriteDescriptorSet descriptorWrite;
+		descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrite.dstSet = m_DescriptorSet;
+		descriptorWrite.dstBinding = 3;
+		descriptorWrite.descriptorCount = 1;
+		descriptorWrite.pImageInfo = &std::static_pointer_cast<TextureVulkan>(texture)->GetDescriptor();
 	}
 
-	void ShaderVulkan::SetEmissiveTexture(std::shared_ptr<Texture> texture) {
-
+	void ShaderVulkan::SetEmissiveTexture(std::shared_ptr<Texture> texture)
+	{
+		VkWriteDescriptorSet descriptorWrite;
+		descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrite.dstSet = m_DescriptorSet;
+		descriptorWrite.dstBinding = 4;
+		descriptorWrite.descriptorCount = 1;
+		descriptorWrite.pImageInfo = &std::static_pointer_cast<TextureVulkan>(texture)->GetDescriptor();
 	}
 
-	void ShaderVulkan::SetMVPBuffer() {
-
+	void ShaderVulkan::SetMVPBuffer()
+	{
 	}
 }
--- Brisk/src/Graphics/Vulkan/TextureVulkan.hpp
+++ Brisk/src/Graphics/Vulkan/TextureVulkan.hpp
@@ -9,32 +9,34 @@
 
 #include <string>
 
-namespace Brisk 
+namespace Brisk
 {
-	class TextureVulkan : public Texture {
+	class TextureVulkan : public Texture
+	{
 	public:
 		VkImageView GetView() { return m_ImageView; }
 		VkImage GetImage() { return m_Image; }
 		VkSampler GetSampler() { return m_Sampler; }
 		VkImage Get() const { return m_Image; }
-		
+
 		virtual uint32_t GetWidth() const override { return m_Width; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
 		virtual void Resize() override {}
 
-		//virtual void Init(int width, int height) override;
+		VkDescriptorImageInfo GetDescriptor() const { return m_Descriptor; }
+
+		// virtual void Init(int width, int height) override;
 		virtual void Init() override;
 		virtual void Init(int width, int height, Core::Format format, Type type) override;
 		virtual void Init(tinygltf::Image image, TextureSampler sampler) override;
-		virtual void Init(const std::string& path) override;
+		virtual void Init(const std::string &path) override;
 
-		//void Create();
+		// void Create();
 		////void Create(int width, int height);
-		//void Create(int width, int height, Format format, Type type);
-		//void Create(const std::string& path);
+		// void Create(int width, int height, Format format, Type type);
+		// void Create(const std::string& path);
 	private:
-
-	private:
+		VkDescriptorImageInfo m_Descriptor;
 		VkImage m_Image;
 		VkImageView m_ImageView;
 		VkDeviceMemory m_Memory;

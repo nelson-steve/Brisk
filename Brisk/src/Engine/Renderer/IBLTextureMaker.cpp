f86e944d7249282e0fa9d39d5eb2d435f356a4ca
Nelson Steve <nelsonsteve.business@gmail.com>
2025-02-24 22:22:16 +0500

    Update

--- Brisk/src/Engine/Renderer/IBLTextureMaker.cpp
+++ Brisk/src/Engine/Renderer/IBLTextureMaker.cpp
@@ -46,14 +46,77 @@ namespace Brisk
 
         m_ComputePipeline->Destroy();
 
-
         OutputTexture->BindAs();
         EnvTexture->BindAs();
 
         CopyImage(OutputTexture, EnvTexture);
 
         OutputTexture->BindAs();
-        EnvTexture->BindAs();
+        EnvTexture->BindAs(); // final cubemap
+
+
+        //
+        std::shared_ptr<DescriptorLayout> materialLayout = DescriptorLayout::Create();
+        materialLayout->pName = "material";
+        materialLayout->AddBindingLayout(0, 1, GPUResource::ResourceType::DESCRIPTOR_TYPE_UNIFORM_BUFFER, { GPUResource::ShaderStageAccess::SHADER_STAGE_VERTEX_BIT });
+        materialLayout->Init();
+
+        std::shared_ptr<DescriptorLayout> pbrLayout = DescriptorLayout::Create();
+        pbrLayout->pName = "pbr";
+        pbrLayout->AddBindingLayout(0, 1, GPUResource::ResourceType::DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, { GPUResource::ShaderStageAccess::SHADER_STAGE_FRAGMENT_BIT });
+        pbrLayout->AddBindingLayout(1, 1, GPUResource::ResourceType::DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, { GPUResource::ShaderStageAccess::SHADER_STAGE_FRAGMENT_BIT });
+        pbrLayout->AddBindingLayout(2, 1, GPUResource::ResourceType::DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, { GPUResource::ShaderStageAccess::SHADER_STAGE_FRAGMENT_BIT });
+        pbrLayout->Init();
+
+        std::shared_ptr<ShaderModule> vertexShaderModule = ShaderModule::Create();
+        vertexShaderModule->Init(std::make_pair("Shaders/Vulkan/Compiled/TriangleVS.spv", Pipeline::ShaderStage::VERTEX));
+
+        std::shared_ptr<ShaderModule> fragmentShaderModule = ShaderModule::Create();
+        fragmentShaderModule->Init(std::make_pair("Shaders/Vulkan/Compiled/TriangleFS.spv", Pipeline::ShaderStage::FRAGMENT));
+
+        Pipeline::GraphicsPipelineSpecs pipelineSpecs{};
+        RenderPass::RenderPassSpecs renderPassSpecs;
+        renderPassSpecs.pAttachments =
+        {
+            {0, Core::Format::FORMAT_B8G8R8A8_UNORM, true, RenderPass::AttachmentType::Swapchain},
+            {1, Core::Format::FORMAT_D32_SFLOAT, true, RenderPass::AttachmentType::Depth}
+        };
+
+        Pipeline::VertexDataLayout vertexLayout;
+        vertexLayout.pBinding = 0;
+        vertexLayout.pStride = sizeof(MeshData);
+        vertexLayout.pAttributes = {
+            {0, 0, Core::Format::FORMAT_R32G32B32_SFLOAT, offsetof(MeshData, MeshData::Position)},
+            {0, 1, Core::Format::FORMAT_R32G32B32_SFLOAT, offsetof(MeshData, MeshData::Normal)},
+            {0, 2, Core::Format::FORMAT_R32G32_SFLOAT,    offsetof(MeshData, MeshData::UV0)},
+            {0, 3, Core::Format::FORMAT_R32G32_SFLOAT,    offsetof(MeshData, MeshData::UV1)},
+            {0, 4, Core::Format::FORMAT_R32G32B32_SFLOAT, offsetof(MeshData, MeshData::Color)},
+        };
+        pipelineSpecs.pLayout = vertexLayout;
+        pipelineSpecs.pRenderPass = RenderPass::Create();
+        pipelineSpecs.pRenderPass->Init(renderPassSpecs);
+
+        pipelineSpecs.pDescriptorLayouts["material"] = materialLayout;
+        pipelineSpecs.pDescriptorLayouts["pbr"] = pbrLayout;
+
+        pipelineSpecs.pShaderModules.push_back(vertexShaderModule);
+        pipelineSpecs.pShaderModules.push_back(fragmentShaderModule);
+
+        pipelineSpecs.pDepthClampEnable = false;
+        pipelineSpecs.pRasterizationDiscardEnable = false;
+        pipelineSpecs.pPolygoneMode = Pipeline::POLYGON_MODE_FILL;
+        pipelineSpecs.pLineWidth = 1.0f;
+        pipelineSpecs.pCullMode = Pipeline::CullMode::BACK;
+        pipelineSpecs.pFrontFace = Pipeline::FrontFace::COUTNER_CLOCKWISE;
+        pipelineSpecs.pDepthBiasEnable = false;
+        pipelineSpecs.pDepthTestEnable = true;
+        pipelineSpecs.pDepthWriteEnable = true;
+        pipelineSpecs.pCompareOp = Pipeline::COMPARE_OP_LESS;
+        pipelineSpecs.pDepthBoundsTestEnable = false;
+        pipelineSpecs.pStencilTestEnable = false;
+
+        pipeline = Pipeline::Create();
+        pipeline->Init(pipelineSpecs);
 	}
 
 	void IBLTextureMaker::EquirectangularMapPath(std::string path) {

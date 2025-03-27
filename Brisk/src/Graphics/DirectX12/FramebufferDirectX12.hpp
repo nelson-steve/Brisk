fdeb353f70708b14f8e03b491398f4b10cf8e791
Nelson Steve <nelsonsteve.business@gmail.com>
2025-03-27 23:33:08 +0500

    Added DirectX12 framebuffer

---
+++ Brisk/src/Graphics/DirectX12/FramebufferDirectX12.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+//INCLUDES
+#include "Engine/Renderer/Framebuffer.hpp"
+//-----------------
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <wrl/client.h>
+//--------------------
+using Microsoft::WRL::ComPtr;
+
+namespace Brisk
+{
+    class FramebufferDirectX12 : public Framebuffer {
+    public:
+        virtual void Init(const FramebufferSpecs& specs) override;
+        virtual void Destroy() override;
+
+        virtual void Bind() override;
+
+    private:
+        Microsoft::WRL::ComPtr<ID3D12Resource> m_RenderTarget;
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_RTVHeap;
+        D3D12_CPU_DESCRIPTOR_HANDLE m_RTVHandle;
+    };
+}
